80f9f69c07dac9ca21323448f85f1bf0d3549882
goaldae <goaldae@naver.com>
2019-01-20 21:27:45 +0900

    study object/array

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-alert('im Working. JS');
+var info = {
+    name: "goaldae",
+    age: 26,
+    gender : "student",
+    favMovies: ["along the god", "LOTR"], //오브젝트 안에 배열을 넣을 수 있음
+}
+
+//배열 안에 오브젝트들을 넣을 수도 있음
+
+console.log(info);
