88ead2b44be02e8c86d25b10939421c40a11e3bb
goaldae <goaldae@naver.com>
2019-01-23 16:02:21 +0900

    practice DOM, event

--- index.js
+++ index.js
@@ -27,3 +27,23 @@ const intro = sayHello2("Nayoung", 24);
 console.log(intro);
 
 //DOM 사용해보기, object개념
+const title = document.getElementById("title"); // document라는 객체로 html의 id 접근
+title.innerHTML = "Hi! from JS!";  // 문자열 수정
+title.style.color = "red";  //색깔 바꾸기
+
+document.title = "change title"; //웹 제목 바꾸기
+
+//title.querySelector("#title"); css선택자처럼 자식 선택해서 바꾸기: 자주쓰임
+
+//자바스크립트는 이벤트에 반응하기 위해 만들어짐
+
+console.dir(document);
+isClick = false;
+//이벤트
+function handleColor(){
+    console.log("changed title color");
+    title.style.color = "blue"; //파란색으로 바꾸기
+
+}
+
+title.addEventListener("click", handleColor);//이때 handleColor()라고 하면 이벤트와 상관없이 실행됨
