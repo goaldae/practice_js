b1e55b04db1e0b654ec45a96220e035080fb92a2
goaldae <goaldae@naver.com>
2019-02-26 09:08:06 +0900

    Make take function

--- range.js
+++ range.js
@@ -24,3 +24,42 @@ L.range = function *(l) { //제너레이터
 const list = L.range(5); //제너레이터를 만듦
 
 log(reduce2(add, list));
+console.clear();
+//원하는 수만큼 자르는 take함수
+const take = curry((l, iter) => {
+    let res = [];
+
+    for(const a of iter){
+        res.push(a);
+        if(res.length == l) return res;
+    }
+    return res;
+});
+
+console.time('');
+log(take(5, range(10000))); //배열을 모두 만든 뒤에 잘라냄
+console.timeEnd('');
+
+console.time('');
+log(take(5, L.range(10000))); //필요한 부분만 만들어냄 (지연성)
+console.timeEnd('');
+
+//이것을 go를 사용해 평가하면
+//curry로 리팩토링 하면 더 간결한 코드 가능
+
+go(
+    L.range(1000),
+    a => take(10, a),
+    log
+);
+
+//curry 이용
+console.time('');
+go(
+    L.range(10000), //무한대를 인자로 해도 상관없음
+    take(10),
+    reduce2(add),
+    log
+);
+console.timeEnd('');
+
