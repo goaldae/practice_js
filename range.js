331d5d8ec4eb387800349f15a16ae832ab30a6f6
goaldae <goaldae@naver.com>
2019-03-14 22:53:50 +0900

    Practice for real code

--- range.js
+++ range.js
@@ -202,3 +202,32 @@ go( //이터러블 프로토콜을 따르는 비동기적방식
     L.map(a=>a*a),
     takeAll,
     log);
+
+
+//실무에서 활용할법하게 바꾼 코드
+
+var users = [
+    { name: 'a', age: 21, family: [
+        { name: 'a1', age: 53 }, { name: 'a2', age: 47 },
+        { name: 'a3', age: 16 }, { name: 'a4', age: 15 }
+        ] },
+    { name: 'b', age: 24, family: [
+        { name: 'b1', age: 58 }, { name: 'b2', age: 51 },
+        { name: 'b3', age: 19 }, { name: 'b4', age: 22 }
+        ] },
+    { name: 'c', age: 31, family: [
+        { name: 'c1', age: 64 }, { name: 'c2', age: 62 }
+        ] },
+    { name: 'd', age: 20, family: [
+        { name: 'd1', age: 42 }, { name: 'd2', age: 42 },
+        { name: 'd3', age: 11 }, { name: 'd4', age: 7 }
+        ] }
+    ];
+
+go(users,
+    L.flatMap(u => u.family),
+    L.filter(u => u.age > 20),
+    L.map(u => u.age),
+    take(4),
+    reduce(add),
+    log);
